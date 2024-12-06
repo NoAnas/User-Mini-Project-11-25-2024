a11dc1e8e6f4140a8cd82eed47a5a71c0eb70ebb
NoAnas <anaswebdev1@gmail.com>
2024-12-07 01:54:36 +0500

    add some changes

--- app.js
+++ app.js
@@ -14,8 +14,47 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
-app.get("/profile", isLoggedIn, (req, res) => {
-  res.render("profile");
+app.get("/profile", isLoggedIn, async (req, res) => {
+  let user = await userModel
+    .findOne({ email: req.user.email })
+    .populate("posts");
+  res.render("profile", { user });
+});
+
+app.get("/like/:id", isLoggedIn, async (req, res) => {
+  let post = await postModel.findOne({ _id: req.params.id }).populate("user");
+  if (post.likes.indexOf(req.user.userid) === -1) {
+    post.likes.push(req.user.userid);
+  } else {
+    post.likes.splice(post.likes.indexOf(req.user.userid), 1);
+  }
+  await post.save();
+  res.redirect("/profile");
+});
+
+app.get("/edit/:id", isLoggedIn, async (req, res) => {
+  let post = await postModel.findOne({ _id: req.params.id }).populate("user");
+  res.render("edit", { post });
+});
+
+app.post("/update/:id", isLoggedIn, async (req, res) => {
+  let post = await postModel.findOneAndUpdate(
+    { _id: req.params.id },
+    { content: req.body.content }
+  );
+  res.redirect("/profile");
+});
+
+app.post("/post", isLoggedIn, async (req, res) => {
+  let user = await userModel.findOne({ email: req.user.email });
+  let { content } = req.body;
+  let post = await postModel.create({
+    user: user._id,
+    content,
+  });
+  user.posts.push(post._id);
+  await user.save();
+  res.redirect("/profile");
 });
 
 app.post("/register", async (req, res) => {
@@ -36,7 +75,7 @@ app.post("/register", async (req, res) => {
         });
         let token = jwt.sign({ email: email, userid: user._id }, "aaa");
         res.cookie("token", token);
-        res.send("registered");
+        res.redirect("profile");
       });
     });
   }
@@ -54,9 +93,9 @@ app.post("/login", async (req, res) => {
     if (result) {
       let token = jwt.sign({ email: email, userid: user._id }, "aaa");
       res.cookie("token", token);
-      res.status(200).send("you can login");
+      res.status(200).redirect("/profile");
     } else {
-      res.redirect("/login");
+      res.render("login");
     }
   });
 });
@@ -69,7 +108,7 @@ app.get("/logout", async (req, res) => {
 //isloggedin middleware
 function isLoggedIn(req, res, next) {
   if (req.cookies.token === "") {
-    res.send("You must be logged in");
+    res.redirect("/login");
   } else {
     let data = jwt.verify(req.cookies.token, "aaa");
     req.user = data;
--- modals/user.js
+++ modals/user.js
@@ -1,14 +1,17 @@
 const mongoose = require("mongoose");
-// const uri =
-//   "mongodb+srv://anas:anas@cluster0.z81xd8p.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-mongoose.connect("mongodb://127.0.0.1:27017/miniproject");
-
+// mongodb://127.0.0.1:27017/miniproject
+// mongoose.connect("mongodb://localhost:27017/miniproject");
+const uri =
+  "mongodb+srv://anas:anas@cluster0.z81xd8p.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+mongoose.connect(uri);
 const userSchema = mongoose.Schema({
   username: String,
   name: String,
   age: Number,
   email: String,
   password: String,
+  posts: [{ type: mongoose.Schema.Types.ObjectId, ref: "post" }],
+  message: String,
 });
 
 module.exports = mongoose.model("user", userSchema);
